3ddbf790ba8bfeeadceb648525e2a766370618e5
James Segarra III <JamesSegarra@gmail.com>
2016-09-29 15:17:26 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(input_array)
+    input_array.each_with_object([]){|word,obj| obj << word if word.chars.sort == @word.chars.sort}
+  end
+
+end
